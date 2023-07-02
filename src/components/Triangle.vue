2083d5de5f57a40413745c712b1eb69d30aaa9cc
Laddge <dev.laddge@gmail.com>
2023-07-02 04:01:52 +0000

    fix pointer events

--- src/components/Triangle.vue
+++ src/components/Triangle.vue
@@ -17,22 +17,22 @@
 
 <template>
   <div class="w-6 h-[2.598rem]">
-    <div class="w-12 h-full relative">
+    <div class="w-12 h-full relative pointer-events-none">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0, 0, 200, 173.2" v-if="failed && mines.includes(n)" class="w-full h-full absolute top-0 left-0 fill-error stroke-[5px] stroke-base-content">
-        <polygon points="100 0, 0 173.2, 200 173.2" v-if="(k ** 2 - n) % 2 == 0" />
-        <polygon points="0 0, 100 173.2, 200 0" v-else />
+        <polygon points="100 0, 0 173.2, 200 173.2" v-if="(k ** 2 - n) % 2 == 0" class="pointer-events-auto" />
+        <polygon points="0 0, 100 173.2, 200 0" v-else class="pointer-events-auto" />
       </svg>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0, 0, 200, 173.2" v-else-if="open.includes(n)" class="w-full h-full absolute top-0 left-0 fill-base-200 stroke-[5px] stroke-base-content">
-        <polygon points="100 0, 0 173.2, 200 173.2" v-if="(k ** 2 - n) % 2 == 0" />
-        <polygon points="0 0, 100 173.2, 200 0" v-else />
+        <polygon points="100 0, 0 173.2, 200 173.2" v-if="(k ** 2 - n) % 2 == 0" class="pointer-events-auto" />
+        <polygon points="0 0, 100 173.2, 200 0" v-else class="pointer-events-auto" />
       </svg>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0, 0, 200, 173.2" v-else-if="flagged.includes(n)" class="w-full h-full absolute top-0 left-0 fill-warning stroke-[5px] stroke-base-content">
-        <polygon points="100 0, 0 173.2, 200 173.2" v-if="(k ** 2 - n) % 2 == 0" @click="action" />
-        <polygon points="0 0, 100 173.2, 200 0" v-else @click="action" />
+        <polygon points="100 0, 0 173.2, 200 173.2" v-if="(k ** 2 - n) % 2 == 0" @click="action" class="pointer-events-auto" />
+        <polygon points="0 0, 100 173.2, 200 0" v-else @click="action" class="pointer-events-auto" />
       </svg>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0, 0, 200, 173.2" v-else class="w-full h-full absolute top-0 left-0 fill-primary/50 stroke-[5px] stroke-base-content">
-        <polygon points="100 0, 0 173.2, 200 173.2" v-if="(k ** 2 - n) % 2 == 0" @click="action" />
-        <polygon points="0 0, 100 173.2, 200 0" v-else @click="action" />
+        <polygon points="100 0, 0 173.2, 200 173.2" v-if="(k ** 2 - n) % 2 == 0" @click="action" class="pointer-events-auto" />
+        <polygon points="0 0, 100 173.2, 200 0" v-else @click="action" class="pointer-events-auto" />
       </svg>
       <div class="w-full h-q/2 absolute top-[0.8rem] left-0 text-center" v-if="(k ** 2 - n) % 2 == 0">
         <span v-if="failed && mines.includes(n)" class="text-error-content">
